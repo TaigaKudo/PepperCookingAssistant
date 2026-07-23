a3b065b89bbce9d9e5d3c030e83422c37cc89ee9
TaigaKudo <tiger1224valentine@gmail.com>
2026-07-24 01:05:44 +0900

    新規プロジェクト追加

---
+++ src/test/java/io/github/TaigaKudo/PepperCookingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.TaigaKudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepperCookingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
