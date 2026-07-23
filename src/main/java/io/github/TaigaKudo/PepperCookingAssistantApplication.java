c09779ef89a77da9522d0ae52b944891095b1010
petaro <95024465+petaro-dev@users.noreply.github.com>
2026-07-24 01:05:44 +0900

    新規プロジェクト追加

---
+++ src/main/java/io/github/TaigaKudo/PepperCookingAssistantApplication.java
@@ -0,0 +1,13 @@
+package io.github.TaigaKudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepperCookingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepperCookingAssistantApplication.class, args);
+	}
+
+}
