c729d3a80c0edc37f193d65ed34141b11b761a9d
petaro <95024465+petaro-dev@users.noreply.github.com>
2026-07-28 00:02:25 +0900

    Entityクラスの新規作成。

---
+++ src/main/java/io/github/TaigaKudo/entity/Ingredient.java
@@ -0,0 +1,53 @@
+package io.github.TaigaKudo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ingredients")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ingredient {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column
+	private String reading;
+	
+	@Column(nullable = false)
+	private String defaultUnit;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id", nullable = false)
+	private IngredientCategory category;
+	
+	@Column(nullable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@Column(nullable = false)
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	@Column
+	private LocalDateTime deletedAt;
+}
