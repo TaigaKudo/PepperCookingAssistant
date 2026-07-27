ac052af1d9797da5e307931aaf15baacb1637ba2
TaigaKudo <tiger1224valentine@gmail.com>
2026-07-28 00:02:25 +0900

    Entityクラスの新規作成。

---
+++ src/main/java/io/github/TaigaKudo/entity/User.java
@@ -0,0 +1,47 @@
+package io.github.TaigaKudo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String passwordHash;
+	
+	@Column(nullable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@Column(nullable = false)
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	@Column
+	private LocalDateTime deletedAt;
+}
